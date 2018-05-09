872a3edbca0cd911cb276fd9004840f325a8fd82
matthi <mruhland01@gmail.com>
2018-05-08 22:15:49 -0700

    updated sidebar / icons / skills

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-<p>portfolio site</p>
+<p><b>portfolio site</b></p>
+
+<p>Portfolio site with a basic single page HTML/CSS front-end and NodeJS express server back-end.</p>
+
+<p>Currently hosted on Heroku at: <a target="_blank" href="https://immense-brook-21804.herokuapp.com/">https://immense-brook-21804.herokuapp.com/</a></p>
+<p>Now also available at: <a target="_blank" href="http://www.matthiasruhland.com">http://www.matthiasruhland.com</a></p>
--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,46 @@
+const toggleSidebar = event => {
+  let sidebar = document.querySelector(".sidebar");
+  let sidebarItems = document.querySelector(".sidebar__items");
 
+  sidebar.classList.toggle("sidebar--active");
 
-const toggleSidebar = () => {
+  if (sidebar.classList.contains("sidebar--active")) {
+    sidebarItems.style.visibility = "visible";
+  } else {
+    sidebarItems.style.visibility = "hidden";
+  }
 
-    let sidebar = document.querySelector(".sidebar");
-    let sidebarItems = document.querySelector(".sidebar__items");
-
-    sidebar.classList.toggle("sidebar--active");
-    
-    if (sidebar.classList.contains("sidebar--active")) {
-        sidebarItems.style.visibility = "visible";
-    } else {
-        sidebarItems.style.visibility = "hidden";
-    }
-    
+  event.stopPropagation();
 };
 
-const sendEmail = (e) => {
-    e.preventDefault();
+const closeSidebar = () => {
+  let sidebar = document.querySelector(".sidebar");
 
-    const email = "mruhland01@gmail.com";
-    const subject = "Portfolio Message";
-    window.open(`mailto:${email}?subject=${subject}`);
+  if (sidebar.classList.contains("sidebar--active")) {
+    sidebar.classList.remove("sidebar--active");
+    document.querySelector(".sidebar__items").style.visibility = "hidden";
+  }
 };
+
+const sendEmail = e => {
+  e.preventDefault();
+
+  const email = "mruhland01@gmail.com";
+  const subject = "Portfolio Message";
+  window.location.href = `mailto:${email}?subject=${subject}`;
+};
+
+// EVENT LISTENERS
+document
+  .querySelector(".sidebar__icon")
+  .addEventListener("click", toggleSidebar, false);
+
+document
+  .getElementById("contact__send-email")
+  .addEventListener("click", sendEmail, false);
+
+document
+  .querySelector(".sidebar__items")
+  .addEventListener("click", event => event.stopPropagation(), false);
+
+document.addEventListener("click", closeSidebar, false);
